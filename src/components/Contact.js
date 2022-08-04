cac79340bb68020675b4b822265018d8b889e953
uday755 <79998578+uday755@users.noreply.github.com>
2022-08-05 00:23:07 +0530

    Added some of changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,21 +7,14 @@ export default function Contact() {
                 <h3 className='text-center text-secondary'><strong>Contact Me For Any Queries </strong></h3>
                 <form className="row g-3 needs-validation" novalidate>
                     <div className="col-md-4">
-                        <label for="validationCustom01" className="form-label">First name</label>
-                        <input type="text" className="form-control" id="validationCustom01"  required />
+                        <label for="validationCustom01" className="form-label">Your Full Name</label>
+                        <input type="text" className="form-control" id="validationCustom01" required />
                         <div className="valid-feedback">
                             Looks good!
                         </div>
                     </div>
                     <div className="col-md-4">
-                        <label for="validationCustom02" className="form-label">Last name</label>
-                        <input type="text" className="form-control" id="validationCustom02"  required />
-                        <div className="valid-feedback">
-                            Looks good!
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <label for="validationCustomUsername" className="form-label">Username</label>
+                        <label for="validationCustomUsername" className="form-label">Username(Gmail Prefered)</label>
                         <div className="input-group has-validation">
                             <span className="input-group-text" id="inputGroupPrepend">@</span>
                             <input type="text" className="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required />
@@ -30,45 +23,37 @@ export default function Contact() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-6">
+                    <div className="col-md-4">
                         <label for="validationCustom03" className="form-label">City</label>
                         <input type="text" className="form-control" id="validationCustom03" required />
                         <div className="invalid-feedback">
                             Please provide a valid city.
                         </div>
                     </div>
-                    <div className="col-md-3">
-                        <label for="validationCustom04" className="form-label">State</label>
+                    <div className="col-md-4">
+                        <label for="validationCustom04" className="form-label">Nationality</label>
                         <select className="form-select" id="validationCustom04" required>
                             <option selected disabled value="">Choose...</option>
                             <option>...</option>
                         </select>
                         <div className="invalid-feedback">
-                            Please select a valid state.
+                            Please select a valid Country.
                         </div>
                     </div>
-                    <div className="col-md-3">
-                        <label for="validationCustom05" className="form-label">Zip</label>
-                        <input type="text" className="form-control" id="validationCustom05" required />
-                        <div className="invalid-feedback">
-                            Please provide a valid zip.
+                    <div className="col-md-12">
+                        <label for="validationCustom01" className="form-label">Your Query</label>
+                        <input type="text" className="form-control" id="validationCustom01" required />
+                        <div className="valid-feedback">
+                            Looks good!
                         </div>
                     </div>
-                    <div className="col-12">
-                        <div className="form-check">
-                            <input className="form-check-input" type="checkbox" value="" id="invalidCheck" required />
-                            <label className="form-check-label" for="invalidCheck">
-                                Agree to terms and conditions
-                            </label>
-                            <div className="invalid-feedback">
-                                You must agree before submitting.
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-12">
-                        <button className="btn btn-primary" type="submit">Submit form</button>
+                   
+                    <div className="col-4">
+                        <button className="btn btn-primary" type="submit">Send Me</button>
+                        <div className='mx-2'>Have Patience . I will definately reply.</div>
                     </div>
                 </form>
+                
             </div>
         </div>
     )
