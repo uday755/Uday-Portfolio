5f57c7d015ddfe5d08cbb1772fce8f0371171981
uday755 <79998578+uday755@users.noreply.github.com>
2024-01-21 12:04:48 +0530

    Added Carousel

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
       <div>
         <Navbar />
-        {/* <BootstrapCarousel /> */}
+        <BootstrapCarousel />
         <Introduction/>
         <Contact/>
       </div>
